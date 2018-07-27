e63a190e017ee1024846c3451b11c2981ac7d1a3
kuskovstanislav <kuskov.stamislav@gmail.com>
2018-07-27 15:16:20 +0300

    Connected to remote car (rpi) and sending commands as 00/speed/angle

--- remoteCarControl.py
+++ remoteCarControl.py
@@ -4,7 +4,6 @@ def send_cmd(command, adress = '172.24.1.1', port = 1080):
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Connect the socket to the port where the CarControl is listening
-    # server_address = ('localhost', 1081)
     server_address = (adress, port)
     sock.connect(server_address)
     try:
